3ef359484945c80a57979d35d649a6fc3e12502c
san ats <san@gmail.com>
2025-02-18 20:09:30 -0500

    Final commit

--- Client/src/router.tsx
+++ Client/src/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./layouts/layout";
-import Products,{Loader as productsLoader} from "./pages/products";
-import NewProduct,{action as newProductoAction} from "./pages/newproduct";
+import Products,{Loader as productsLoader} from "./pages/Products";
+import NewProduct,{action as newProductoAction} from "./pages/Newproduct";
 import EditProduct ,{Loader as editproductLoader , action as editProductAcion} from "./pages/EditProduct";
 import { action as deleteProduct} from "./components/ProductDetails";
 export const router = createBrowserRouter([
